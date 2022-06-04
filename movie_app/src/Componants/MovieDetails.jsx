9770c70981875afa445fb0ccf85759f78586c761
Priyanka mahale <mahalepriyanka82@gmail.com>
2022-06-04 13:48:44 +0530

    change

--- movie_app/src/Componants/MovieDetails.jsx
+++ movie_app/src/Componants/MovieDetails.jsx
@@ -9,9 +9,9 @@ function MovieDetails({
     return(
         <div className="movie" style={{display:"flex",margin:"20px",boxShadow: "rgba(0, 0, 0, 0.1) 0px 4px 12px",backgroundColor:"black",color:"white",width:"500px",height:"150px",alignItems:"center",marginLeft:"500px"}}>
             <img style={{width:"120px",height:"120px",margin:"10px"}} src={image} alt="movieImage" />
-            <h3>Movie Name: {movie_name}</h3>
+            <h3>Name: {movie_name}</h3>
             <div style={{marginLeft:"20px",marginRight:"20px"}}>
-            <p style={{color:"red"}}>Rating: ★{rating}</p>
+            <p style={{backgroundColor:"green",width:"40px",height:"20px"}}>★{rating}</p>
             <p>Date: {released_data}</p>
             </div>
             
